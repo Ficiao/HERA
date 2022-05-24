d7bee44c3172f9696b59ccaff5602a02810f6db1
petrafribert <petra.fribert@fer.hr>
2022-05-25 00:25:23 +0200

    remove test code

--- HERA/Main.cpp
+++ HERA/Main.cpp
@@ -1,7 +1,6 @@
 #include "DataLoader.h"
 #include "GraphNode.h"
 #include "ConnectionsSorter.h"
-#include "Connection.h"
 
 int main() {
     int nodesSize = 9284;
@@ -10,19 +9,6 @@ int main() {
     GraphNode* _readNodes = _dataLoader->LoadData("overlap_reads_rc_ava_pb.paf", NULL, false, nodesSize);
 	GraphNode* _contigNodes = _dataLoader->LoadData("overlaps_reads_contigs_rc_ava_pb.paf", _readNodes, true, 6);
 
-    GraphNode* _testNodes = new GraphNode[2];
-    GraphNode* _oneNode = &_testNodes[0];
-    GraphNode* _twoNode = &_testNodes[1];
-
-    Connection* _connectionOne = new Connection();
-    Connection* _connectionTwo = new Connection();
-    _connectionOne->overlapScore = 10;
-    _connectionTwo->overlapScore = 5;
-
-    _twoNode->connections.push_back(*_connectionTwo);
-    _twoNode->connections.push_back(*_connectionOne);
-
     ConnectionsSorter* _connectionsSorter = new ConnectionsSorter();
-    _connectionsSorter->sortConnectionsByOverlapScore(_testNodes, 2);
-    printf("Done");
+    _connectionsSorter->sortConnectionsByOverlapScore(_readNodes, nodesSize);
 }
