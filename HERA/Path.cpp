cbf96248ea7c2f3d063e47c55c44a3f5be3674b4
Ficiao <filipmtvn@gmail.com>
2022-05-25 12:01:07 +0200

    Added templates for path creation and path selection from buckets

---
+++ HERA/Path.cpp
@@ -0,0 +1,13 @@
+#include "Path.h"
+
+using namespace std;
+
+bool Path::CreateDeterministicPath(GraphNode* _readNodes, GraphNode* _contigNode, int _indexOfStartingRead) {
+
+	return false;
+}
+
+bool Path::CreateMonteCarloPath(GraphNode* _readNodes, GraphNode* _contigNode, int _indexOfStartingRead) {
+
+	return false;
+}
