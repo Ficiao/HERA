7464b67c4ebf9215574fc23a288681d1b422e4b0
Ficiao <filipmtvn@gmail.com>
2022-05-25 17:20:48 +0200

    Finished deterministic path creation

--- HERA/Path.cpp
+++ HERA/Path.cpp
@@ -4,15 +4,58 @@
 using namespace std;
 
 bool Path::CreateDeterministicPath(GraphNode* _readNodes, GraphNode* _contigNode, int _indexOfStartingRead) {
-	pathNodes.push_back(_contigNode);
-	pathLength = pathLength + _contigNode->connections.at(_indexOfStartingRead).baseEnd;
+	_contigNode->hasBeenUsed = true;
 
+	//todo: dodati da se postavi na true i od komplementa
 
-	return false;
+	//pathLength = pathLength + _contigNode->connections.at(_indexOfStartingRead).baseEnd;
+
+	pathNodes.insert(pathNodes.begin(), _contigNode);
+
+	if (RekurzCreateDeterministicPath(_contigNode->connections.at(_indexOfStartingRead).target) == true) {
+		pathNodes.insert(pathNodes.begin(), _contigNode);
+		averageSequenceIdentity = _contigNode->connections.at(_indexOfStartingRead).sequenceIdentity;
+		nuberOfNodes++;
+
+		for (int i = 0; i < pathNodes.size(); i++) {
+			pathNodes.at(i)->hasBeenUsed = false;
+		}
+
+		return true;
+	}
+
+	return false;;
 }
 
 bool Path::RekurzCreateDeterministicPath(GraphNode* _currentNode) {
+	bool _pathCreated;
 
+	_currentNode->hasBeenUsed = true;
+
+	for (int i = 0; i < _currentNode->backwardsContigConnection.size(); i++) {
+		if (_currentNode->backwardsContigConnection.at(i).target->hasBeenUsed == false) {
+			pathNodes.insert(pathNodes.begin(), _currentNode->backwardsContigConnection.at(i).target);
+			averageSequenceIdentity = _currentNode->backwardsContigConnection.at(i).sequenceIdentity;
+			nuberOfNodes++;
+			//treba li dodati i duljinu contiga?
+			return true;
+		}
+	}
+
+
+	for (int i = 0; i < _currentNode->connections.size(); i++) {
+		if (_currentNode->connections.at(i).target->hasBeenUsed == false) {
+			_pathCreated = RekurzCreateDeterministicPath(_currentNode->connections.at(i).target);
+			if (_pathCreated == true) {
+				pathNodes.insert(pathNodes.begin(), _currentNode);
+				averageSequenceIdentity = _currentNode->connections.at(i).sequenceIdentity;
+				nuberOfNodes++;
+				pathLength += _currentNode->size - _currentNode->connections.at(i).baseEnd;
+			}
+		}
+	}
+
+	_currentNode->hasBeenUsed = false;
 	return false;
 }
 
--- HERA/Path.h
+++ HERA/Path.h
@@ -6,8 +6,14 @@ class Path {
 public:
 	int pathLength;
 	int nuberOfNodes;
-	double avergaeSequenceIdentity;
+	double averageSequenceIdentity;
 	std::vector<GraphNode*> pathNodes;
+
+	Path() {
+		pathLength = 0;
+		nuberOfNodes = 0;
+		averageSequenceIdentity = -1;
+	}
 
 	bool CreateDeterministicPath(GraphNode* _readNodes, GraphNode* _contigNode, int _indexOfStartingRead);
 	bool CreateMonteCarloPath(GraphNode* _readNodes, GraphNode* _contigNode, int _indexOfStartingRead);
