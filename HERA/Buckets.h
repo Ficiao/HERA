72c6743bde8287dca938d064725aff267c318620
Ficiao <filipmtvn@gmail.com>
2022-05-26 21:38:53 +0200

    Made path creation to ignore creating paths to complement node of the current node

--- HERA/Buckets.h
+++ HERA/Buckets.h
@@ -7,9 +7,16 @@ public:
 
 	Buckets(int _numberOfContigs) {
 		for (int i = 1; i <= _numberOfContigs; i++) {
-			for (int j = 1; j < _numberOfContigs; j++) {
-				if (j != i) {
-					buckets.push_back(*new Bucket(i, j));
+			for (int j = 1; j <= _numberOfContigs; j++) {
+				if (i <= _numberOfContigs / 2) {
+					if (j != i && i != j - (_numberOfContigs / 2)) {
+						buckets.push_back(*new Bucket(i, j));
+					}
+				}
+				else {
+					if (j != i && i != j + (_numberOfContigs / 2)) {
+						buckets.push_back(*new Bucket(i, j));
+					}
 				}
 			}			
 		}
